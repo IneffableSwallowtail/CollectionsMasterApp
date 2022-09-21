7aeeaaf337a8ffe986995c879bead6dba28be079
A.J. Miller <ajmiller1206@gmail.com>
2022-09-21 14:37:17 -0400

    first commit

---
+++ CollectionsMasterConsoleUI/Methods.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace CollectionsMasterConsoleUI
+{
+    internal class Methods
+    {
+        public static int[] AssignRandom(int[] array)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(1, array.Length);
+            }
+            return array;
+        }
+        public static List<int> AssignRandom(List<int> randomInts)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 50; i++)
+            {
+                randomInts.Add(rnd.Next(1, 49));
+            }
+            return randomInts;
+        }
+        public static void ArrayPrint(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                Console.Write($"{array[i]}, ");
+            }
+            Console.WriteLine(array[array.Length - 1]);
+        }
+        public static void ListPrint(List<int> items)
+        {
+            if (items.Count > 0)
+            {
+                for (int i = 0; i < (items.Count - 1); i++)
+                {
+                    Console.Write($"{items[i]}, ");
+                }
+                Console.WriteLine(items[items.Count - 1]);
+            }
+        }
+        public static void ThreesToZero(int[] array)
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                array[i] = (array[i] % 3 == 0) ? 0 : array[i];
+                if (i < 49) Console.Write($"{array[i]}, ");
+            }
+            Console.WriteLine(array[array.Length - 1]);
+        }
+        public static void PrintIfPresent(int i, List<int> items)
+        {
+            bool hasValue = items.Contains(i);
+            if (hasValue == true)
+                ListPrint(items);
+        }
+        public static void Evens(List<int> integers)
+        {
+            for (int i = 0; i < integers.Count; i++)
+            {
+                int num = integers[i];
+                if (num % 2 == 1)
+                {
+                    integers.Remove(num);
+                    i--;
+                }
+            }
+            ListPrint(integers);
+        }
+    }
+}
--- CollectionsMasterConsoleUI/Program.cs
+++ CollectionsMasterConsoleUI/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace CollectionsMasterConsoleUI
 {
@@ -13,18 +16,20 @@ namespace CollectionsMasterConsoleUI
 
             #region Arrays
             //TODO: Create an integer Array of size 50
-            
+            int[] _50Random = new int[50];
 
             //TODO: Create a method to populate the number array with 50 random numbers that are between 0 and 50
-            
+            _50Random = Methods.AssignRandom(_50Random);
 
             //TODO: Print the first number of the array
+            Console.WriteLine($"First number in random array: {_50Random[0]}");
 
-            //TODO: Print the last number of the array            
+            //TODO: Print the last number of the array
+            Console.WriteLine($"Last number in random array: {_50Random[_50Random.Length - 1]}");
 
-            Console.WriteLine("All Numbers Original");
+            Console.WriteLine("All Numbers Original:");
             //UNCOMMENT this method to print out your numbers from arrays or lists
-            //NumberPrinter();
+            NumberPrinter(_50Random);
             Console.WriteLine("-------------------");
 
             //TODO: Reverse the contents of the array and then print the array out to the console.
@@ -32,8 +37,10 @@ namespace CollectionsMasterConsoleUI
             /*  1) First way, using a custom method => Hint: Array._____(); 
                 2) Second way, Create a custom method (scroll to bottom of page to find ⬇⬇⬇)
             */
-
+            Array.Reverse(_50Random);
             Console.WriteLine("All Numbers Reversed:");
+            Methods.ArrayPrint(_50Random);
+            Array.Reverse(_50Random); //returning the array to its original order - AJ
 
             Console.WriteLine("---------REVERSE CUSTOM------------");
 
@@ -41,14 +48,17 @@ namespace CollectionsMasterConsoleUI
 
             //TODO: Create a method that will set numbers that are a multiple of 3 to zero then print to the console all numbers
             Console.WriteLine("Multiple of three = 0: ");
-            
-
+            int[] noThrees = new int[50];
+            Array.Copy(_50Random, noThrees, 50);
+            Methods.ThreesToZero(noThrees);
             Console.WriteLine("-------------------");
 
             //TODO: Sort the array in order now
             /*      Hint: Array.____()      */
-            Console.WriteLine("Sorted numbers:");
-            
+            Array.Sort(_50Random);
+
+            Console.WriteLine("Sorted numbers: ");
+            Methods.ArrayPrint(_50Random);
 
             Console.WriteLine("\n************End Arrays*************** \n");
             #endregion
@@ -58,46 +68,69 @@ namespace CollectionsMasterConsoleUI
 
             /*   Set Up   */
             //TODO: Create an integer List
-            
+            List<int> integers = new List<int>();
+
 
             //TODO: Print the capacity of the list to the console
-            
+            Console.WriteLine($"Capacity of integers list: {integers.Capacity}");
 
             //TODO: Populate the List with 50 random numbers between 0 and 50 you will need a method for this            
-            
+            Methods.AssignRandom(integers);
 
             //TODO: Print the new capacity
-            
+            Console.WriteLine($"The new capacity for the integers list is {integers.Capacity}.");
 
             Console.WriteLine("---------------------");
 
             //TODO: Create a method that prints if a user number is present in the list
             //Remember: What if the user types "abc" accident your app should handle that!
-            Console.WriteLine("What number will you search for in the number list?");
-            
+            Console.WriteLine("Search for a number between (and excluding) 0 and 50.");
+            Console.WriteLine($"What number will you search for in the number list?");
+            bool cont = false;
+            while (true)
+            {
+                int searchKey = 0;
+                try
+                {
+                    searchKey = Int32.Parse(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                }
+                if (searchKey > 0 && searchKey < 50)
+                    break;
+                else
+                    Console.WriteLine("Invalid selection");
+            }
+            //Methods.PrintIfPresent(searchKey, integers);
             Console.WriteLine("-------------------");
 
             Console.WriteLine("All Numbers:");
             //UNCOMMENT this method to print out your numbers from arrays or lists
             //NumberPrinter();
+            Methods.ListPrint(integers);
             Console.WriteLine("-------------------");
 
 
             //TODO: Create a method that will remove all odd numbers from the list then print results
             Console.WriteLine("Evens Only!!");
+            Methods.Evens(integers);
             
             Console.WriteLine("------------------");
 
             //TODO: Sort the list then print results
             Console.WriteLine("Sorted Evens!!");
+            integers.Sort();
+            Methods.ListPrint(integers);
             
             Console.WriteLine("------------------");
 
             //TODO: Convert the list to an array and store that into a variable
-            
+            int[] ints = integers.ToArray();
 
             //TODO: Clear the list
-            
+            integers.Clear();
+
 
             #endregion
         }
